c38fa99636f0c62b9cbaa7a95312d8dacf9ef020
TingWei-Chuang1 <qsdsz456@gmail.com>
2025-07-10 10:11:01 +0800

    PSD to HTML

---
+++ zuHause/Controllers/TenantController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace zuhause.Controllers
+{
+    public class TenantController : Controller
+    {
+        public IActionResult FrontPage()
+        {
+            return View();
+        }
+
+        public IActionResult Announcement()
+        {
+            return View();
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+        public IActionResult CollectionAndComparison()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ zuHause/Models/CollectionAndComparisonModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace zuhause.Models // 再次確認，命名空間必須是 zuhause.Models
+{
+    public class CollectionAndComparisonModel : PageModel
+    {
+        // 如果您暫時不需要任何屬性或方法，這裡可以保持空白，但類別本身必須存在
+        public void OnGet()
+        {
+            // 頁面加載時的初始化邏輯
+        }
+    }
+}
