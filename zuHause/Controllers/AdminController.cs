56e34f448dbfce1a385aa79d568390fc5770aab6
wes248 <wjw92900@gmail.com>
2025-07-12 15:47:01 +0800

    - 會員總表的靜態切板 - 完成partial view分離

---
+++ zuHause/Controllers/AdminController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace zuHause.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult admin_userslist()
+        {
+            return View();
+        }
+
+
+    }
+}
